00f57c8cfc41173f9662fae41749d0c4e59be47c
jadit19 <jadit19@gmail.com>
2024-02-23 20:35:44 +0530

    elementary zmq consumer implementation

---
+++ consumer.py
@@ -0,0 +1,22 @@
+import os
+import dotenv
+import stripe
+import zmq
+
+dotenv.load_dotenv()
+
+stripe.api_key = os.getenv("STRIPE_API_KEY")
+
+context: zmq.Context = zmq.Context()
+zmq_socket = context.socket(zmq.PULL)
+zmq_socket.bind("tcp://127.0.0.1:5555")
+
+while True:
+    message: dict = zmq_socket.recv_json()
+    if not set(["id", "name", "email"]).issubset(message.keys()):
+        continue
+
+    customer = stripe.Customer.create(
+        id=message.get("id"), name=message.get("name"), email=message.get("email")
+    )
+    print(customer)
